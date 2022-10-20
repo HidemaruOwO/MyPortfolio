98b9239fbbbab0f4b6c528b6b7574156667bec9c
HidemaruOwO <hidemal@outlook.jp>
2022-10-21 07:04:29 +0900

    :sparkles: 一幕目の追加 (public/image/square_slime.png src/components/Index.tsx)

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,21 +1,33 @@
 import react from "react";
 import Header from "./Header";
+import { Row, Col } from "react-bootstrap";
 
 export default () => {
   return (
     <div>
       <Header />
       <div id="introduction" className="bg-white h-96 text-purple-mine p-8">
-        <h2 className="font-bold text-xl sm:text-5xl m-2">
+        <h2 className="font-bold text-xl sm:text-5xl">
           こんにちは。ひでまるです！！
         </h2>
-        <p className="sm:text-xl">
-          プログラミングが好きな中学生のポートフォリオです。
-          <br />
-          とてもかわいくサイトを作ったのですが、どうでしたか？
-          <br />
-          私としてはかなり好みなページができました！！
-        </p>
+        <Row className="mt-12 lg:mt-20" >
+          <Col>
+            <p className="text-base sm:text-xl">
+              プログラミングが好きな中学生のポートフォリオです。
+              <br />
+              とてもかわいくサイトを作ったのですが、どうでしたか？
+              <br />
+              私としてはかなり好みなページができました！！
+            </p>
+          </Col>
+          <Col>
+            <img
+              src="/image/square_slime.png"
+              alt="丸いスライムアイコン"
+              className="h-48 w-48"
+            />
+          </Col>
+        </Row>
       </div>
       <div className="bg-purple-mine h-96 text-slate-50"></div>
     </div>
