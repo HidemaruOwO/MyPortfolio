240b6f94e2c3fb6fab65e167204c858a129c8ab9
HidemaruOwO <hidemal@outlook.jp>
2022-10-21 01:16:52 +0900

    :art: ヘッダーの完成 (public/font/rounded-mplus-1m-bold.ttf public/font/rounded-mplus-1m-medium.ttf src/components/Header.tsx src/components/Index.tsx src/components/Layout.astro src/pages/index.astro src/styles/global.css)

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import react from "react";
+
+export default () => {
+  return (
+    <div>
+      <div className="h-56 sm:h-96 min-w-full text-gray-900 ">
+        <h3 className="font-round-medium text-lg sm:text-xl mx-16 mt-4 sm:mt-8 text-right">
+          <nav>ぷよっとした</nav>
+          <nav>かわいいポートフォリオ</nav>
+        </h3>
+
+        <h1 className="mt-3 sm:mt-14 mx-14 text-3xl sm:text-7xl font-round-bold">
+          <nav>ひでまるの</nav>
+          <nav>　 ポートフォリオ</nav>
+        </h1>
+        <a
+          className="btn btn-border font-round-bold mt-4 sm:mt-12 mx-24 sm:mx-12"
+          href="/#introduction"
+        >
+          自己紹介
+        </a>
+      </div>
+      <div className="bg-cyan-700 h-16"></div>
+    </div>
+  );
+};
--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,10 +1,14 @@
 import react from "react";
+import Header from "./Header";
 
 export default () => {
   return (
     <div>
-      <h1>Wekcome to my portfolio!!</h1>
-      <h2>ぷよぷよ</h2>
+      <Header />
+      <div id="introduction" className="bg-white h-96 text-purple-mine p-8">
+        <h2 className="font-bold text-xl sm:text-5xl">こんにちは。ひでまるです！！</h2>
+      </div>
+      <div className="bg-purple-mine h-96 text-slate-50"></div>
     </div>
   );
 };
