a566903b6b54b372624a97dd3caf45b39cfb5d69
HidemaruOwO <hidemal@outlook.jp>
2022-10-21 06:41:50 +0900

    :wrench: bootstrapの追加 (package.json src/components/Header.tsx src/components/Index.tsx src/components/Layout.astro yarn.lock)

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -6,7 +6,16 @@ export default () => {
     <div>
       <Header />
       <div id="introduction" className="bg-white h-96 text-purple-mine p-8">
-        <h2 className="font-bold text-xl sm:text-5xl">こんにちは。ひでまるです！！</h2>
+        <h2 className="font-bold text-xl sm:text-5xl m-2">
+          こんにちは。ひでまるです！！
+        </h2>
+        <p className="sm:text-xl">
+          プログラミングが好きな中学生のポートフォリオです。
+          <br />
+          とてもかわいくサイトを作ったのですが、どうでしたか？
+          <br />
+          私としてはかなり好みなページができました！！
+        </p>
       </div>
       <div className="bg-purple-mine h-96 text-slate-50"></div>
     </div>
