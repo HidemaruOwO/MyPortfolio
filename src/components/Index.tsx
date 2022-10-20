164c1163df6401d6724416f37dc00c648de76e94
HidemaruOwO <hidemal@outlook.jp>
2022-10-21 07:17:18 +0900

    :sparkles: 二幕目の追加 (src/components/Index.tsx)

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -10,7 +10,7 @@ export default () => {
         <h2 className="font-bold text-xl sm:text-5xl">
           こんにちは。ひでまるです！！
         </h2>
-        <Row className="mt-12 lg:mt-20" >
+        <Row className="mt-12 lg:mt-20">
           <Col>
             <p className="text-base sm:text-xl">
               プログラミングが好きな中学生のポートフォリオです。
@@ -30,7 +30,20 @@ export default () => {
           </Col>
         </Row>
       </div>
-      <div className="bg-purple-mine h-96 text-slate-50"></div>
+      <div className="bg-purple-mine h-96 text-slate-50 p-8">
+        <h2 className="font-bold text-xl sm:text-5xl">何をしているの？</h2>
+        <div className="mt-12 lg:mt-20">
+          <p className="text-base sm:text-xl">
+            普段はプログラミングとデザインを主軸にして趣味を展開してます。
+            <br />
+            プログラミングに関してですが、好きな言語はGoとRustであり。好きなエディターはVimが好きです。
+            <br />
+            デザインに関しては、このようなモダンなウェブページを作成したり。または壁紙を作ったり。ブログのOGPを作ったりして楽しんでます。
+            <br />
+            このサイトは上のプログラミングとデザインの一貫として作成しました。
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
